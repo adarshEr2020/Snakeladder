1e40a3b851717ae81a01008550ac243989bc9bfc
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-09 16:54:22 +0530

    adding game start player position 0

--- SnakeLadder/src/SnakeLadder.java
+++ SnakeLadder/src/SnakeLadder.java
@@ -1,10 +1,11 @@
 
 public class SnakeLadder {
-
+	
 	public static void main(String[] args) {
 
 		System.out.println("welcome to snake ladder game :");
-
+		int playerPosition = 0;
+		System.out.println("player start position = " +playerPosition);
 	}
 
 }
