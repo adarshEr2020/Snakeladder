4cc7c59dde9d9e233760813674e26ebe182c59fe
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-20 15:51:58 +0530

    ADD : single player at start position 0

--- src/com/snake/SnakeLadder.java
+++ src/com/snake/SnakeLadder.java
@@ -4,7 +4,8 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake Ladder Game : ");
-
+		int playerPosition = 0;
+		System.out.println("Single player start position : " +playerPosition);
 	}
 
 }
