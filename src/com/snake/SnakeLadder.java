5774fe984d174858f2d4a5a122fdf3b965e11212
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-20 21:11:06 +0530

    ADD : check Option No Play, Ladder or Snake.

--- src/com/snake/SnakeLadder.java
+++ src/com/snake/SnakeLadder.java
@@ -1,48 +1,43 @@
 package com.snake;
 
 public class SnakeLadder {
-// constants
+	// constants
+
 	public static final int NO_PLAY = 0;
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
 
-	public static int playerPosition = 0;
-	
-	// dice roll and check conditions
-	public void dice() {
-		int diceRoll = (int) (Math.random()*6)+1;
-		System.out.println("diceRoll = " +diceRoll);
-
-		int option = (int) (Math.random()*3);
-		System.out.println("option : " +option);
-
-		switch(option) {
-		// no play position 
-		case NO_PLAY :
-			playerPosition += 0; 
-			break;
-		
-		// if ladder moves ahead 
-		case LADDER :
-			playerPosition += diceRoll;
-			break;
-			
-		// if snake moves behind
-		case SNAKE :
-			if(playerPosition < 0)
-			playerPosition -= diceRoll;
-			break;
-		}
-		System.out.println("player position : " +playerPosition);
-	}
-
 	// main here 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake Ladder Game : ");
-		System.out.println("Single player start position : " +playerPosition); 
-		SnakeLadder snakeladder = new SnakeLadder();  // object
+		int playerPosition = 0;
+		System.out.println("playerPosition " +playerPosition);
 		
-		snakeladder.dice(); // calling d
-	}
+		int winningPosition = 100;
+		for(playerPosition = 0; playerPosition < winningPosition; playerPosition++) {
+			int diceRoll = (int) (Math.random()*6)+1;
+			int option = (int) (Math.random()*3);
 
+			if(playerPosition < 0) {
+				switch(option) {
+				case NO_PLAY: 
+					playerPosition = 0;
+					//System.out.println("Player position is: "+playerPosition);
+					break;
+				case LADDER:
+					playerPosition += diceRoll;
+					//System.out.println("Player position is: "+playerPosition);
+					break;
+				case SNAKE:
+					playerPosition -= diceRoll;
+					break;
+
+				}
+
+			}
+
+		}
+		System.out.println("playerPosition = " +playerPosition);
+	}
 }
+
