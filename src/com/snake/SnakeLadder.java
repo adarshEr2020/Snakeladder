d821ea423ba5fb5cb4e38251d9adb54f2d799b63
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-20 16:37:10 +0530

    Add : dice roll between o to 6

--- src/com/snake/SnakeLadder.java
+++ src/com/snake/SnakeLadder.java
@@ -1,11 +1,17 @@
 package com.snake;
 
 public class SnakeLadder {
+	
+	public static void dice() {
+		int diceRoll = (int) (Math.random()*6)+1;
+		System.out.println("diceRoll = " +diceRoll);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake Ladder Game : ");
 		int playerPosition = 0;
 		System.out.println("Single player start position : " +playerPosition);
+		dice();
 	}
 
 }
