fce4a122fd0b24796b099546bf5625da5d03aa56
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-20 17:06:01 +0530

    Add : check No_play , Ladder and Snake

--- src/com/snake/SnakeLadder.java
+++ src/com/snake/SnakeLadder.java
@@ -1,17 +1,48 @@
 package com.snake;
 
 public class SnakeLadder {
+// constants
+	public static final int NO_PLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
+
+	public static int playerPosition = 0;
 	
-	public static void dice() {
+	// dice roll and check conditions
+	public void dice() {
 		int diceRoll = (int) (Math.random()*6)+1;
 		System.out.println("diceRoll = " +diceRoll);
+
+		int option = (int) (Math.random()*3);
+		System.out.println("option : " +option);
+
+		switch(option) {
+		// no play position 
+		case NO_PLAY :
+			playerPosition += 0; 
+			break;
+		
+		// if ladder moves ahead 
+		case LADDER :
+			playerPosition += diceRoll;
+			break;
+			
+		// if snake moves behind
+		case SNAKE :
+			if(playerPosition < 0)
+			playerPosition -= diceRoll;
+			break;
+		}
+		System.out.println("player position : " +playerPosition);
 	}
 
+	// main here 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Snake Ladder Game : ");
-		int playerPosition = 0;
-		System.out.println("Single player start position : " +playerPosition);
-		dice();
+		System.out.println("Single player start position : " +playerPosition); 
+		SnakeLadder snakeladder = new SnakeLadder();  // object
+		
+		snakeladder.dice(); // calling d
 	}
 
 }
