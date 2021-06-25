284c7a1a0621782ad7aea4f4eb6c055b63968f5f
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-25 23:52:52 +0530

    Added : Exact winning position

--- src/com/snake/SnakeLadder.java
+++ src/com/snake/SnakeLadder.java
@@ -6,38 +6,54 @@ public class SnakeLadder {
 	public static final int NO_PLAY = 0;
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
+	static int playerPosition = 0;
+	int winningPosition = 100;
 
-	// main here 
-	public static void main(String[] args) {
-		System.out.println("Welcome To Snake Ladder Game : ");
-		int playerPosition = 0;
-		System.out.println("playerPosition " +playerPosition);
-		
-		int winningPosition = 100;
+	// dicecheck method
+	public void diceCheck() {
 		for(playerPosition = 0; playerPosition < winningPosition; playerPosition++) {
 			int diceRoll = (int) (Math.random()*6)+1;
 			int option = (int) (Math.random()*3);
 
-			if(playerPosition < 0) {
-				switch(option) {
-				case NO_PLAY: 
-					playerPosition = 0;
-					//System.out.println("Player position is: "+playerPosition);
-					break;
-				case LADDER:
-					playerPosition += diceRoll;
-					//System.out.println("Player position is: "+playerPosition);
-					break;
-				case SNAKE:
+			switch(option) {
+			case NO_PLAY: 
+				playerPosition = 0;
+				System.out.println("Player position is: "+playerPosition);
+				break;
+			case LADDER:
+				playerPosition += diceRoll;
+				//System.out.println("Player position is: "+playerPosition);
+				if(playerPosition > 100) {
 					playerPosition -= diceRoll;
-					break;
-
+					System.out.println("After ladder playerPosition = " +playerPosition);
 				}
+				
+				break;
+			case SNAKE:
+				playerPosition -= diceRoll;
+				System.out.println("After snake playerPosition = " +playerPosition);
+				break;
 
 			}
-
+			
 		}
-		System.out.println("playerPosition = " +playerPosition);
+		System.out.println("win playerPosition = " +playerPosition);
+	}
+	
+
+	public static void main(String[] args) {
+		SnakeLadder snakeLadder = new SnakeLadder();
+		System.out.println("Welcome To Snake Ladder Game : ");
+		System.out.println("playerPosition " +playerPosition);
+		snakeLadder.diceCheck();
+		
+		// if completed 100 repeat from start 
+		if(playerPosition > 100) {
+			snakeLadder.diceCheck();
+		}else if(playerPosition == 100) {
+			snakeLadder.diceCheck();
+		}else {
+			snakeLadder.diceCheck();
+		}
 	}
 }
-
